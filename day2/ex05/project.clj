947ef4db6efedd4e93f00b4ec4bb856bf8603a99
Karsten Schmidt <k@postspectacular.com>
2016-03-23 18:06:30 +0000

    update & document worker example (day2/ex05)

--- day2/ex05/project.clj
+++ day2/ex05/project.clj
@@ -4,22 +4,36 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [reagent "0.5.1"]]
 
   :plugins      [[lein-figwheel "0.5.0-6"]
                  [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
 
   :clean-targets ^{:protect false} ["resources/public/js" "target"]
-  
+
   :cljsbuild {:builds
               [{:source-paths ["src"]
                 :id "dev"
                 :compiler {:optimizations :simple
                            :pretty-print  true
-                           ;;:pseudo-names  true
+                           :output-to     "resources/public/js/main.js"
+                           :modules       {:cljs-base {:output-to "resources/public/js/base.js"}
+                                           :app       {:output-to "resources/public/js/app.js"
+                                                       :entries #{"ex05.core"
+                                                                  ;;"reagent.core" "React"
+                                                                  }}
+                                           :worker    {:output-to "resources/public/js/worker.js"
+                                                       :entries #{"worker"}
+                                                       ;;:depends-on #{}
+                                                       }}}}
+               {:source-paths ["src"]
+                :id "prod"
+                :compiler {:optimizations :advanced
                            :output-to     "resources/public/js/main.js"
                            :modules       {:cljs-base {:output-to "resources/public/js/base.js"}
                                            :app       {:output-to "resources/public/js/app.js"
                                                        :entries #{"ex05.core"}}
                                            :worker    {:output-to "resources/public/js/worker.js"
-                                                       :entries #{"worker"}}}}}]})
+                                                       :entries #{"worker"}}}}}
+               ]})
--- day2/ex05/src/ex05/worker.cljs
+++ day2/ex05/src/ex05/worker.cljs
@@ -1,13 +1,39 @@
-(ns worker)
+(ns worker
+  (:require
+   [cljs.reader :refer [read-string]]))
 
+;; base.js contains all of cljs.core etc.
 (.importScripts js/self "base.js")
 
-(enable-console-print!)
+;; worker's app state
+(def state (atom {}))
 
-(set! (.-onmessage js/self)
-      (fn [msg]
-        (prn "worker received: " (.-data msg))))
+(defn start-ping
+  "Command handler to start (and keep) pinging main app with given
+  interval. Pinging stops when :active? key is false."
+  [delay]
+  (js/setTimeout
+   (fn []
+     (.postMessage
+      js/self
+      (str "Worker running (" (js/Date.) ")"))
+     (when (:active? @state)
+       (start-ping delay)))
+   delay)
+  (swap! state assoc :active? true))
 
-(js/setInterval
- (fn [] (prn "worker running..."))
- 1000)
+(defn stop-ping
+  "Command handler to stop pinging main app."
+  [] (swap! state assoc :active? false))
+
+(defn dispatch-command
+  "Worker's onmessage handler. Decodes message as EDN and dispatches
+  to command handlers based on :command key in message."
+  [msg]
+  (let [msg (read-string (.-data msg))]
+    (case (keyword (:command msg))
+      :start (start-ping (:interval msg))
+      :stop  (stop-ping)
+      (.warn js/console (str "unknown worker command: " (:command msg))))))
+
+(set! (.-onmessage js/self) dispatch-command)
